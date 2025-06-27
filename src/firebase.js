cceb3b4b51316280116ee2e3c334d736267b833e
ananovdesign <ananov.krasimir@gmail.com>
2025-06-27 16:16:56 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,11 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC9MnO6FeXeInWWuSaqHFojnc57dH3K6s4",
+  authDomain: "dyt-ops-app.firebaseapp.com",
+  projectId: "dyt-ops-app",
+  storageBucket: "dyt-ops-app.firebasestorage.app",
+  messagingSenderId: "987453203064",
+  appId: "1:987453203064:web:31a83fe1fc03658b47438d"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db }
