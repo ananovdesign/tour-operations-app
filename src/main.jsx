c198551d1a3f2b43e089005aa1bc7600d9eecc39
ananovdesign <ananov.krasimir@gmail.com>
2026-01-07 12:31:06 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppV2 from './v2/AppV2.jsx' // Зареждаме новата модулна версия
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppV2 />
+    <App />
   </React.StrictMode>,
-)
