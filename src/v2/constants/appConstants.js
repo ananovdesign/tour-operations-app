dcedcacf304b0f03f93824b28567e460c4d17ead
ananovdesign <ananov.krasimir@gmail.com>
2026-01-07 14:22:50 +0200

    Create appConstants.js

---
+++ src/v2/constants/appConstants.js
@@ -0,0 +1,12 @@
+export const TABS = {
+    DASHBOARD: 'Dashboard',
+    RESERVATIONS: 'Reservations',
+    INVOICES: 'Invoices',
+    FINANCIALS: 'FinancialS',
+    TASKS: 'Tasks',
+    MARKETING: 'Marketing',
+    DOCUMENTS: 'Documents'
+};
+
+export const TASK_STATUSES = ['Planned', 'Completed', 'Cancelled']; // От TaskManagementModule
+export const CAMPAIGN_STATUSES = ['Planned', 'Active', 'Completed', 'Paused']; // От MarketingHubModule
