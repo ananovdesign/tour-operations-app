ddff2a18560fc7b992ccb704196f0040f0e9647b
ananovdesign <ananov.krasimir@gmail.com>
2026-02-02 13:17:49 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './v3/AppV3.jsx'
+import App from './v3/app.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
